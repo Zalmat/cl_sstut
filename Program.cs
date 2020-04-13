f4adda075e752c09780b02f7c55275d8a279e1b6
Zalmat <zalmat@yandex.ru>
2020-04-13 22:41:42 +0500

    Мелкая правка

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace cl_sstut
             string text = "100-13579-26&5000&0&1676473&c550eb5d-cad1-4e01-a98e-2dc702e3b630&927138ef-f298-4631-9727-248bcb5dd565&false&14400&card&none&SHOP_TOKEN&SHOP_SEC_KEY";
             // ответ 0DB584FB679B0D9DC2EBBD9D4B2E9191
             //Проверка шифрования
-            Console.WriteLine(HashText.CkassaMD5(text));
+
+            Console.WriteLine("Расчёт со строки: " + HashText.CkassaMD5(text));
             
             //проверка всего алгоритма
             //string[] proverka = new string[] {"111aaaa", "222aaaa", "333aaaa", "4444aaaa"};
@@ -29,7 +30,7 @@ namespace cl_sstut
                "none"              
             };
             SignatureGeneration sg = new SignatureGeneration(shopToken: "SHOP_TOKEN", shopSecKey: "SHOP_SEC_KEY");
-            Console.WriteLine( sg.GetSign(proverka));
+            Console.WriteLine("Расчёт классом:   "+ sg.GetSign(proverka));
            
         } 
        
